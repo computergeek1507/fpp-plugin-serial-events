d22fd7b61272730ff22ba04fc89ed39e33d07479
Scott Hanson <scooter_seh@yahoo.com>
2023-08-31 19:36:37 -0400

    working on php

--- plugin_setup.php
+++ plugin_setup.php
@@ -1,6 +1,5 @@
 
 <?
-
 function returnIfExists($json, $setting) {
     if ($json == null) {
         return "";
@@ -20,17 +19,17 @@ function convertAndGetSettings() {
         $json = json_decode($j, true);
         return $json;
     }
-    $j = "{\"ports\": [], \"events\": [] }";
+    $j = "[]";
     return json_decode($j, true);
 }
 
 $pluginJson = convertAndGetSettings();
-
 ?>
 
 
 <div id="global" class="settings">
-<legend>Serial Event Control Config</legend>
+<fieldset>
+<legend>FPP Serial Event Config</legend>
 
 <script>
 
@@ -55,251 +54,149 @@ if (is_dir("/dev/serial/by-id")) {
 }
 ?>
 
-allowMultisyncCommands = true;
+var serialEventConfig = <? echo json_encode($pluginJson, JSON_PRETTY_PRINT); ?>;
 
-function AddOption(value, text, current) {
-    var o = "<option value='" + value + "'";
-    var realVal = $('<textarea />').html(value).text();
-
-    if (value == current || realVal == current)
-        o += " selected";
-
-    o += ">" + text + "</option>";
-
-    return o;
-}
-
-function RemoveCondition(item) {
-    if ($(item).parent().find('tr').length == 1)
-        return;
-
-    $(item).remove();
-}
-
-function AddCondition(row, condition, compare, text) {
-    var rows = $(row).find('.conditions > tr').length;
-    var c = "<tr>";
-
-    if (rows == 0)
-        c += "<td><button class='circularButton circularButton-vsm circularButton-sm circularButton-visible circularAddButton' onClick='AddCondition($(this).parent().parent().parent().parent(), \"ALWAYS\", \"\", \"\");'>Add</button></td>";
-    else
-        c += "<td><button class='circularButton circularButton-vsm circularButton-sm circularButton-visible circularDeleteButton' onClick='RemoveCondition($(this).parent().parent());'>Delete</button></td>";
-
-    c += "<td><select class='conditionSelect'>";
-    c += AddOption('b1', 'Byte 1', condition);
-    c += AddOption('b2', 'Byte 2', condition);
-    c += AddOption('b3', 'Byte 3', condition);
-    c += AddOption('b4', 'Byte 4', condition);
-    c += AddOption('b5', 'Byte 5', condition);
-    c += AddOption('noteOn', 'Note On', condition);
-    c += AddOption('noteOff', 'Note Off', condition);
-    c += AddOption('velocity', 'Velocity', condition);
-    c += AddOption('channel', 'Channel', condition);
-    c += AddOption('control', 'Control Change', condition);
-    c += AddOption('pitch', 'Pitch Change', condition);
-    c += "</select>";
-
-    c += "<select class='conditionTypeSelect'>";
-    c += AddOption('=', '=', compare);
-    c += AddOption('!=', '!=', compare);
-    c += AddOption('&lt;', '&lt;', compare);
-    c += AddOption('&lt;=', '&lt;=', compare);
-    c += AddOption('&gt;', '&gt;', compare);
-    c += AddOption('&gt;=', '&gt;=', compare);
-    c += "</select>";
-
-    c += "<input type='text' size='12' maxlength='30' class='conditionText' value='" + text + "'>";
-
-    c += "</td></tr>";
-
-    $(row).find('.conditions').append(c);
-}
 
 var uniqueId = 1;
-function AddMIDI() {
-    var id = $("#midiEventTableBody > tr").length + 1;
-    
+var modelOptions = "";
+function AddSerialEventItem(type) {
+    var id = $("#serialeventTableBody > tr").length + 1;
     var html = "<tr class='fppTableRow";
     if (id % 2 != 0) {
         html += " oddRow'";
     }
-    html += "'><td class='center' valign='middle'><div class='rowGrip'><i class='rowGripIcon fpp-icon-grip'></i></div></td>";
-    html += "<td><input type='text' size='30' maxlength='50' class='desc'><span style='display: none;' class='uniqueId'>" + uniqueId + "</span></td>";
-    html += "<td><table><tbody class='conditions'></tbody></table>";
-    html += "</td><td><table class='fppTable' border=0 id='tableMIDICommand_" + uniqueId +"'>";
-    html += "<tr><td>Command:</td><td><select class='midicommand' id='midicommand" + uniqueId + "' onChange='CommandSelectChanged(\"midicommand" + uniqueId + "\", \"tableMIDICommand_" + uniqueId + "\" , false, PrintArgsInputsForEditable);'><option value=''></option></select></td></tr>";
-    html += "</table></td></tr>";
+    html += "'><td class='colNumber rowNumber'>" + id + ".</td><td><span style='display: none;' class='uniqueId'>" + uniqueId + "</span></td>";
+
+    html += DeviceSelect(SerialDevices, "");
     
-    $("#midiEventTableBody").append(html);
-    LoadCommandList($('#midicommand' + uniqueId));
+    html += "<td><input type='text' minlength='7' maxlength='15' size='15' class='descrip' /></td>";
+    html += "<td><select class='conditiontype'>";
+    html += "<option value='contains'";
+    if(type == 'contains') {html += " selected ";}
+    html += ">Contains</option><option value='startswith'";
+    if(type == 'startswith') {html += " selected ";}
+    html += ">Starts With</option><option value='endswith'";
+    if(type == 'endswith') {html += " selected ";}
+    html += ">Ends With</option><option value='regex'";
+    if(type == 'regex') {html += " selected ";}
+    html += ">Regex</option></select>";
+    html += "<td><input type='text'  minlength='7' maxlength='15' size='15' class='conditionvalue' />";
+    html += "</td><td><table class='fppTable' border=0 id='tableSerialCommand_" + uniqueId +"'>";
+    html += "<td>Command:</td><td><select class='serialcommand' id='serialcommand" + uniqueId + "' onChange='CommandSelectChanged(\"serialcommand" + uniqueId + "\", \"tableSerialCommand_" + uniqueId + "\" , false, PrintArgsInputsForEditable);'><option value=''></option></select></td></tr>";
+    html += "</table></td></tr>";
+    //selected
+    $("#serialeventTableBody").append(html);
 
-    newRow = $('#midiEventTableBody > tr').last();
-    $('#midiEventTableBody > tr').removeClass('selectedEntry');
+    LoadCommandList($('#serialcommand' + uniqueId));
+
+    newRow = $('#serialeventTableBody > tr').last();
+    $('#serialeventTableBody > tr').removeClass('selectedEntry');
     DisableButtonClass('deleteEventButton');
-
     uniqueId++;
 
     return newRow;
 }
 
-function RemoveMIDI() {
-    if ($('#midiEventTableBody').find('.selectedEntry').length) {
-        $('#midiEventTableBody').find('.selectedEntry').remove();
-    }
+function SaveSerialEventItem(row) {
+    var ip = $(row).find('.ipaddress').val();
+    var startchan = parseInt($(row).find('.startchan').val(),10);
 
-    DisableButtonClass('deleteEventButton');
+    var plugnum = parseInt($(row).find('.plugnum').val(),10);
+	var conditiontype = $(row).find('.conditiontype').val();
+
+    var json = {
+        "ip": ip,
+        "startchannel": startchan,
+        "condition": conditiontype,
+        "plugnumber": plugnum
+    };
+    return json;
 }
 
-var midiConfig = <? echo json_encode($pluginJson, JSON_PRETTY_PRINT); ?>;
-function SaveMIDIConfig(config) {
-    var data = JSON.stringify(config);
+function SaveSerialEventItems() {
+    var serialeventConfig = [];
+    var i = 0;
+    $("#serialeventTableBody > tr").each(function() {
+        serialeventConfig[i++] = SaveSerialEventItem(this);
+    });
+    
+    var data = JSON.stringify(serialeventConfig);
     $.ajax({
         type: "POST",
-        url: 'api/configfile/plugin.serial-event',
+	url: 'api/configfile/plugin.serial-event.json',
         dataType: 'json',
         async: false,
         data: data,
         processData: false,
         contentType: 'application/json',
         success: function (data) {
+           SetRestartFlag(2);
         }
     });
-    SetRestartFlag(2);
 }
 
-function SaveEvent(row) {
-    var desc = $(row).find('.desc').val();
-    var conditions = [];
 
-    $(row).find('.conditions > tr').each(function() {
-        var cond     = $(this).find('.conditionSelect').val();
-        var condType = $(this).find('.conditionTypeSelect').val();
-        var condText = $(this).find('.conditionText').val();
+function RenumberRows() {
+    var id = 1;
+    $('#serialeventTableBody > tr').each(function() {
+        $(this).find('.rowNumber').html('' + id++ + '.');
+        $(this).removeClass('oddRow');
 
-        var condition = {};
-        condition.condition = cond;
-        condition.conditionCompare = condType;
-        condition.conditionText = condText;
-        conditions.push(condition);
-    });
-
-    var id = $(row).find('.uniqueId').html();
-    
-    var json = {
-        "description": desc,
-        "conditions": conditions
-    };
-    CommandToJSON('midicommand' + id, 'tableMIDICommand_' + id, json, true);
-    return json;
-}
-
-function SavePort(row) {
-    var enabled = $(row).find('.enabled').is(':checked');
-    var sysEx = $(row).find('.sysEx').is(':checked');
-    var timeCode = $(row).find('.timeCode').is(':checked');
-    var sense = $(row).find('.sense').is(':checked');
-    var name =  $(row).find('.port').html();
-    
-    var json = {
-        "name": name,
-        "enabled": enabled,
-        "enableSysEx": sysEx,
-        "enableTimeCode": timeCode,
-        "enableSense": sense
-    };
-    return json;
-}
-function SaveMIDI() {
-    midiConfig = { "ports": [], "events": []};
-    var i = 0;
-    $("#midiPortTableBody > tr").each(function() {
-        midiConfig["ports"][i++] = SavePort(this);
-    });
-    i = 0;
-    $("#midiEventTableBody > tr").each(function() {
-        midiConfig["events"][i++] = SaveEvent(this);
-    });
-    
-    SaveMIDIConfig(midiConfig);
-}
-function RefreshLastMessages() {
-    $.get('api/plugin-apis/MIDI/Last', function (data) {
-          $("#lastMessages").text(data);
+        if (id % 2 != 0) {
+            $(this).addClass('oddRow');
         }
-    );
+    });
 }
+function RemoveSerialEventItem() {
+    if ($('#serialeventTableBody').find('.selectedEntry').length) {
+        $('#serialeventTableBody').find('.selectedEntry').remove();
+        RenumberRows();
+    }
+    DisableButtonClass('deleteEventButton');
+}
+
 
 $(document).ready(function() {
-
-    $('#midiEventTableBody').sortable({
+                  
+    $('#serialeventTableBody').sortable({
         update: function(event, ui) {
+            RenumberRows();
         },
         item: '> tr',
         scroll: true
     }).disableSelection();
 
-    $('#midiEventTableBody').on('mousedown', 'tr', function(event,ui){
-        $('#midiEventTableBody tr').removeClass('selectedEntry');
+    $('#serialeventTableBody').on('mousedown', 'tr', function(event,ui){
+        $('#serialeventTableBody tr').removeClass('selectedEntry');
         $(this).addClass('selectedEntry');
         EnableButtonClass('deleteEventButton');
     });
-
 });
 
 </script>
+<div>
+<table border=0>
+<tr><td colspan='2'>
+        <input type="button" value="Save" class="buttons genericButton" onclick="SaveSerialEventItems();">
+        <input type="button" value="Add" class="buttons genericButton" onclick="AddSerialEventItem('light');">
+        <input id="delButton" type="button" value="Delete" class="deleteEventButton disableButtons genericButton" onclick="RemoveSerialEventItem();">
+    </td>
+</tr>
+</table>
 
+<div class='fppTableWrapper fppTableWrapperAsTable'>
+<div class='fppTableContents'>
+<table class="fppTable" id="serialeventTable"  width='100%'>
+<thead><tr class="fppTableHeader"><th>#</th><th></th><th>Description</th><th>Condition Type</th><th>Condition Value</th><th>Command</th></tr></thead>
+<tbody id='serialeventTableBody'>
+</tbody>
+</table>
+</div>
 
-
-<div class="row">
-    <div class="col-auto mr-auto">
-        <div class="row">
-            <div class="col-auto">
-                <div class="fppTableWrapper fppTableWrapperAsTable">
-                    <div class="fppTableContents" role="region">
-                        <table class="fppSelectableRowTable">
-                            <thead>
-                                <tr>
-                                    <th style="min-width:60px">Enable</th>
-                                    <th style="padding-right: 15px;">MIDI Device</th>
-                                    <th style="min-width:60px">SysEx</th>
-                                    <th style="min-width:60px">Time</th><th>Sense</th>
-                                </tr>
-                            </thead>
-                            <tbody id='midiPortTableBody'>
-                                <? foreach ($portsAvail as $port) { ?>
-                                <tr><td><input type="checkbox" class="enabled"></td>
-                                    <td class="port" style="padding-right: 15px;"><?= $port; ?></td>
-                                    <td><input type="checkbox" class="sysEx"></td>
-                                    <td><input type="checkbox" class="timeCode"></td>
-                                    <td><input type="checkbox" class="sense"></td></tr>
-                                <? } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div classs="row">
-            <div class="col-auto">
-                <input type="button" value="Save" class="buttons genericButton" onclick="SaveMIDI();">
-                <input type="button" value="Add" class="buttons genericButton" onclick="AddCondition(AddMIDI(), 'ALWAYS', '', '');">
-                <input id="delButton" type="button" value="Delete" class="deleteEventButton disableButtons genericButton" onclick="RemoveMIDI();">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-auto">
-                <div class='fppTableWrapper'>
-                    <div class='fppTableContents'>
-                        <table  class="fppSelectableRowTable" id="midiEventTable"  width='100%'>
-                            <thead><tr class="fppTableHeader"><th>#</th><th>Description</th><th>Conditions</th><th>Command</th></tr></thead>
-                            <tbody id='midiEventTableBody' class="ui-sortable"></tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-auto">
+</div>
+<div>
+<p>
+<div class="col-auto">
         <div>
             <div class="row">
                 <div class="col">
@@ -313,38 +210,18 @@ $(document).ready(function() {
             </div>
         </div>
     </div>
+<p>
 </div>
-
-
-
+</div>
 <script>
-$.each(midiConfig["events"], function( key, val ) {
-    var row = AddMIDI();
-    $(row).find('.desc').val(val["description"]);
 
-    for (var i = 0; i < val['conditions'].length; i++) {
-        AddCondition(row,
-            val['conditions'][i]['condition'],
-            val['conditions'][i]['conditionCompare'],
-            val['conditions'][i]['conditionText']);
-    }
-    var id = parseInt($(row).find('.uniqueId').html());
-    PopulateExistingCommand(val, 'midicommand' + id, 'tableMIDICommand_' + id, false, PrintArgsInputsForEditable);
+$.each(serialeventConfig, function( key, val ) {
+    var row = AddSerialEventItem(val["conditiontype"]);
+    $(row).find('.ipaddress').val(val["ip"]);
+    $(row).find('.startchan').val(val["startchannel"]);
+    $(row).find('.plugnum').val(val["plugnumber"]);
 });
-
-$.each(midiConfig["ports"], function( key, val ) {
-    $("#midiPortTableBody > tr").each(function() {
-        var name =  $(this).find('.port').html();
-        if (name == val["name"]) {
-            $(this).find('.enabled').prop('checked', val["enabled"]);
-            $(this).find('.sysEx').prop('checked', val["enableSysEx"]);
-            $(this).find('.timeCode').prop('checked', val["enableTimeCode"]);
-            $(this).find('.sense').prop('checked', val["enableSense"]);
-        }
-    });
-});
-
-RefreshLastMessages();
 </script>
-</div>
 
+</fieldset>
+</div>
