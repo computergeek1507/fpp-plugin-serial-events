8fe9dcd2ff11672d8f5184db3124f85c307dd8a0
Scott Hanson <scooter_seh@yahoo.com>
2023-09-07 15:02:31 -0400

    switch to callback

--- src/SerialEventPlugin.cpp
+++ src/SerialEventPlugin.cpp
@@ -135,20 +135,17 @@ public:
     std::vector<std::unique_ptr<SerialEvent>> serial_events;
     std::list<std::string> serial_data;
     
-    std::mutex queueLock;
-    std::atomic_int m_fd{0};
-
-    std::promise<void> exitSignal;
-    std::unique_ptr<std::thread> th;
+    int m_fd {-1};
+    bool enabled {false};
   
     SerialEventPlugin() : FPPPlugin("fpp-plugin-serial_event") {
         LogInfo(VB_PLUGIN, "Initializing Serial Event Plugin\n");        
-        InitSerial();
+        enabled = InitSerial();
     }
     virtual ~SerialEventPlugin() {   
         CloseSerial();     
     }
-    void InitSerial() {
+    bool InitSerial() {
         if (FileExists(FPP_DIR_CONFIG("/plugin.serial-event.json"))) {
             std::string port;
             int speed = 115200;
@@ -170,7 +167,7 @@ public:
                 LogInfo(VB_PLUGIN, "Using %s Serial Output Speed %d\n", port.c_str(), speed);
                 if(port.empty()) {
                     LogErr(VB_PLUGIN, "Serial Port is empty '%s'\n", port.c_str());
-                    return;
+                    return false;
                 }
                 if(port.find("/dev/") == std::string::npos)
                 {
@@ -179,34 +176,27 @@ public:
                 int fd = SerialOpen(port.c_str(), speed, "8N1", false);
                 if (fd < 0) {
                     LogErr(VB_PLUGIN, "Could Not Open Serial Port '%s'\n", port.c_str());
-                    return;
+                    return false;
                 }
                 m_fd = fd;
                 std::this_thread::sleep_for(std::chrono::milliseconds(10));
                 tcflush(m_fd,TCIOFLUSH);
                 ioctl(m_fd, TCIOFLUSH, 2); 
-                std::future<void> futureObj = exitSignal.get_future();
-                // Starting Thread & move the future object in lambda function by reference
-                th.reset(new std::thread(&SerialEventPlugin::threadFunction, this, std::move(futureObj)));
                 LogInfo(VB_PLUGIN, "Serial Input Started\n");
-
+                return true;
             } catch (...) {
                 LogErr(VB_PLUGIN, "Could not Initialize Serial Port '%s'\n", port.c_str());
             }                
         }else{
             LogInfo(VB_PLUGIN, "No plugin.serial-event.json config file found\n");
         }
+        return false;
     }
 
     void CloseSerial() {
-        if(th) {
-            exitSignal.set_value();
-            //Wait for thread to join
-            th->join();
-            th.reset(nullptr);            
-        }
-        if (m_fd != 0) {
+        if (m_fd >= 0) {
             SerialClose(m_fd);
+            m_fd = -1;
         }
     }
 
@@ -228,47 +218,6 @@ public:
     void remove_control_characters(std::string& s) {
         s.erase(std::remove_if(s.begin(), s.end(), [](char c) { return std::iscntrl(c); }), s.end());
     }
-
-    void threadFunction(std::future<void> futureObj) {
-        LogInfo(VB_PLUGIN, "Serial Thread Start\n");
-        std::cout << "Thread Start" << std::endl;
-        while (futureObj.wait_for(std::chrono::milliseconds(100)) == std::future_status::timeout)
-        {
-            if (m_fd <= 0) {
-                LogInfo(VB_PLUGIN, "Serial Is Zero\n");
-            }
-            //LogInfo(VB_PLUGIN, "Serial Doing Some Work\n");
-            //std::cout << "Doing Some Work" << std::endl;
-            if(SerialDataAvailable(m_fd))
-            {
-                char buf[256];
-                int n = read(m_fd,&buf, sizeof(buf));
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                tcflush(m_fd, TCIOFLUSH);
-                ioctl(m_fd, TCIOFLUSH, 2); 
-                std::string serialData = buf;
-
-                remove_control_characters(serialData);
-                if(!serialData.empty()) {
-                    LogInfo(VB_PLUGIN, "Serial data found '%s'\n", serialData.c_str());
-                    serial_data.push_back(serialData);
-                    if (serial_data.size() > 25) {
-                        serial_data.pop_front();
-                    }
-                    for (auto &a : serial_events) {
-                        if (a->matches(serialData)) {
-                            a->invoke(serialData);
-                        }
-                    }
-                }
-                
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        }
-        LogInfo(VB_PLUGIN, "Serial Thread End\n");
-        std::cout << "Thread End" << std::endl;
-    }
-
     virtual const std::shared_ptr<httpserver::http_response> render_GET(const httpserver::http_request &req) override {
         std::string v;
         
@@ -285,6 +234,43 @@ public:
     }
     void registerApis(httpserver::webserver *m_ws) override {
         m_ws->register_resource("/SERIALEVENT", this, true);
+    }
+
+     virtual void addControlCallbacks(std::map<int, std::function<bool(int)>> &callbacks) {
+        if (enabled) {
+            std::function<bool(int)> fn = [this](int d) {
+                char buffer[100];
+                while(1) {
+                    ssize_t length = read(m_fd, &buffer, sizeof(buffer));
+                    if (length == -1) {
+                        break;
+                    } else if (length == 0) {
+                        break;
+                    } else {
+                        buffer[length] = '\0';
+                    }
+                }
+
+                std::string serialData = buffer;
+
+                remove_control_characters(serialData);
+                if(!serialData.empty()) {
+                    LogInfo(VB_PLUGIN, "Serial data found '%s'\n", serialData.c_str());
+                    serial_data.push_back(serialData);
+                    if (serial_data.size() > 25) {
+                        serial_data.pop_front();
+                    }
+                    for (auto &a : serial_events) {
+                        if (a->matches(serialData)) {
+                            a->invoke(serialData);
+                        }
+                    }
+                }
+ 
+            return false;
+        };
+        callbacks[m_fd] = fn;
+        }
     }
 };
 
